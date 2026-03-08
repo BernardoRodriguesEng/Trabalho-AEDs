cdb4a2cda498c5ee7441a1c2f356f586f7f44e06
BernardoRodriguesEng <bernardo.rp.eng@gmail.com>
2026-03-08 15:48:36 -0300

    Arquivo .csv convertido para binario, porém sem otimização de espaço.

--- main.cpp
+++ main.cpp
@@ -4,5 +4,9 @@
 using namespace std;
 
 int main() {
+    ler_arquivo_csv leitor;
+    cout << "Iniciando conversao..." << endl;
+    leitor.converter_csv_binario();
+    cout << "Conversao concluida com sucesso!" << endl;
     return 0;
 }
--- transformar-binario.cpp
+++ transformar-binario.cpp
@@ -30,20 +30,26 @@ struct Game {
     float price;
 };
 
+void safelySetString(char* dest, const string& src, int size){
+    memset(dest, 0, size);
+    strncpy(dest, src.c_str(), size - 1);
+}
 
-
-class transformar_binario{
+class ler_arquivo_csv{
     private:
-        ifstream csvFile("steam.csv");
+        ifstream csvFile;
         string line;
 
     public:
-        transformar_binario();
-        ~transformar_binario();
-        void transformar();
+        ler_arquivo_csv();
+        ~ler_arquivo_csv();
+        Game linha_csv();
+        bool fim_arquivo();
+        void converter_csv_binario();
 };
 
-transformar_binario::transformar_binario(){
+ler_arquivo_csv::ler_arquivo_csv(){
+    csvFile.open("steam.csv");
     if (!csvFile.is_open()) {
         cerr << "Erro ao abrir o arquivo CSV!" << endl;
         return;
@@ -51,35 +57,52 @@ transformar_binario::transformar_binario(){
     getline(csvFile, line);
 }
 
-transformar_binario::~transformar_binario(){
-    
+ler_arquivo_csv::~ler_arquivo_csv(){
+    csvFile.close();
+    csvFile.clear();
+    line.clear();
 }
 
-void transformar_binario::transformar(){
-    while (getline(csvFile, line)) {
-        stringstream ss(line);
-        string field;
-        Game g;
-        getline(ss, field, ','); g.appid = stoi(field);
-        getline(ss, field, ','); safelySetString(g.name, field, sizeof(g.name));
-        getline(ss, field, ','); safelySetString(g.release_date, field, sizeof(g.release_date));
-        getline(ss, field, ','); g.english = (field == "1");
-        getline(ss, field, ','); safelySetString(g.developer, field, sizeof(g.developer));
-        getline(ss, field, ','); safelySetString(g.publisher, field, sizeof(g.publisher));
-        getline(ss, field, ','); safelySetString(g.platforms, field, sizeof(g.platforms));
-        getline(ss, field, ','); g.required_age = stoi(field);
-        getline(ss, field, ','); safelySetString(g.categories, field, sizeof(g.categories));
-        getline(ss, field, ','); safelySetString(g.genres, field, sizeof(g.genres));
-        getline(ss, field, ','); safelySetString(g.steamspy_tags, field, sizeof(g.steamspy_tags));
-        getline(ss, field, ','); g.achievements = stoi(field);
-        getline(ss, field, ','); g.positive_ratings = stoi(field);
-        getline(ss, field, ','); g.negative_ratings = stoi(field);
-        getline(ss, field, ','); g.average_playtime = stoi(field);
-        getline(ss, field, ','); g.median_playtime = stoi(field);
-        getline(ss, field, ','); safelySetString(g.owners, field, sizeof(g.owners));
-        getline(ss, field, ','); g.price = stof(field);
 
+Game ler_arquivo_csv::linha_csv(){
+    Game g;
+    getline(csvFile, line);
+    stringstream ss(line);
+    string field;
+    getline(ss, field, ','); g.appid = stoi(field);
+    getline(ss, field, ','); safelySetString(g.name, field, sizeof(g.name));
+    getline(ss, field, ','); safelySetString(g.release_date, field, sizeof(g.release_date));
+    getline(ss, field, ','); g.english = (field == "1");
+    getline(ss, field, ','); safelySetString(g.developer, field, sizeof(g.developer));
+    getline(ss, field, ','); safelySetString(g.publisher, field, sizeof(g.publisher));
+    getline(ss, field, ','); safelySetString(g.platforms, field, sizeof(g.platforms));
+    getline(ss, field, ','); g.required_age = stoi(field);
+    getline(ss, field, ','); safelySetString(g.categories, field, sizeof(g.categories));
+    getline(ss, field, ','); safelySetString(g.genres, field, sizeof(g.genres));
+    getline(ss, field, ','); safelySetString(g.steamspy_tags, field, sizeof(g.steamspy_tags));
+    getline(ss, field, ','); g.achievements = stoi(field);
+    getline(ss, field, ','); g.positive_ratings = stoi(field);
+    getline(ss, field, ','); g.negative_ratings = stoi(field);
+    getline(ss, field, ','); g.average_playtime = stoi(field);
+    getline(ss, field, ','); g.median_playtime = stoi(field);
+    getline(ss, field, ','); safelySetString(g.owners, field, sizeof(g.owners));
+    getline(ss, field, ','); g.price = stof(field);
+    return g;
+}
 
-        
+bool ler_arquivo_csv::fim_arquivo(){
+    return csvFile.eof();
+}
+
+void ler_arquivo_csv::converter_csv_binario(){
+    ofstream arquivoBin("steam.bin", ios::binary);
+    if(!arquivoBin.is_open()){
+            cerr << "Erro ao criar o arquivo Binario!" << endl;
     }
-}
+    Game g;
+    while (!fim_arquivo()) {
+        g = linha_csv();
+        arquivoBin.write(reinterpret_cast<char*>(&g), sizeof(Game));
+    }
+    arquivoBin.close();
+}   
